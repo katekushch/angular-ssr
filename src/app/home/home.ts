5e7ab11850da826180f1c552a4427f5441d3f61b
Kate Kushch <katya@2muchcoffee.com>
2025-08-18 13:13:14 +0300

    feat(i18n): update home component to use translation for the title

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PageWrapperComponent } from "../components/page-wrapper/page-wrapper";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.html',
     styleUrls: ['./home.scss'],
-    imports: [PageWrapperComponent]
+    imports: [PageWrapperComponent, TranslateModule],
 })
 export class HomeComponent {
     
