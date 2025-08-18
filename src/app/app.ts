8a8d91d7766af1fc7f7d19a9e80fa12b39258d03
Kate Kushch <katya@2muchcoffee.com>
2025-08-18 13:13:03 +0300

    feat(i18n): integrate TranslateService into app component for language support

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'public';
+  private translate = inject(TranslateService);
+
+    constructor() {
+        this.translate.use('en');
+    }
 }
