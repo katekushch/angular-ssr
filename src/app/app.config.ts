eb0cd5a2217b928fd2f04fd448f6ea6bc7fbef12
Kate Kushch <katya@2muchcoffee.com>
2025-08-18 13:12:53 +0300

    feat(i18n): configure translation service with JsonFileLoader and fallback language

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { routes } from './app.routes';
+import { provideTranslateLoader, provideTranslateService } from '@ngx-translate/core';
+import { JsonFileLoader } from './core/i18n/json-file-loader';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), 
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideTranslateService({
+      loader: provideTranslateLoader(JsonFileLoader),
+      fallbackLang: 'en'
+    })
   ]
 };
